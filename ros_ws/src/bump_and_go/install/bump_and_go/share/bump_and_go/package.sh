51a72e312ef90abb80b60f081f259b09d8e09869
naytrCP <ntrubalc@calpoly.edu>
2025-10-28 16:36:39 -0700

    bump_and_go

--- ros_ws/src/bump_and_go/install/bump_and_go/share/bump_and_go/package.sh
+++ ros_ws/src/bump_and_go/install/bump_and_go/share/bump_and_go/package.sh
@@ -52,7 +52,7 @@ _colcon_prepend_unique_value() {
 # since a plain shell script can't determine its own path when being sourced
 # either use the provided COLCON_CURRENT_PREFIX
 # or fall back to the build time prefix (if it exists)
-_colcon_package_sh_COLCON_CURRENT_PREFIX="/gobilda_sim/ros_ws/src/bump_and_go/install/bump_and_go"
+_colcon_package_sh_COLCON_CURRENT_PREFIX="/home/calpoly/Desktop/CPE416/ros_ws/src/bump_and_go/install/bump_and_go"
 if [ -z "$COLCON_CURRENT_PREFIX" ]; then
   if [ ! -d "$_colcon_package_sh_COLCON_CURRENT_PREFIX" ]; then
     echo "The build time path \"$_colcon_package_sh_COLCON_CURRENT_PREFIX\" doesn't exist. Either source a script for a different shell or set the environment variable \"COLCON_CURRENT_PREFIX\" explicitly." 1>&2
