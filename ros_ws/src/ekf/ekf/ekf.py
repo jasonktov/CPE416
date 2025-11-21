a1c48e0b18e4e7ff2e808bb6a970fa243037029d
Jason Tov <tov@calpoly.edu>
2025-11-21 14:01:10 -0800

    rev3

--- ros_ws/src/ekf/ekf/ekf.py
+++ ros_ws/src/ekf/ekf/ekf.py
@@ -85,9 +85,7 @@ class Filter(Node):
         timer_period = 0.05 # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
-        self.state = np.array([[0],
-                               [0],
-                               [0]])
+        self.state = np.array([0, 0, 0])
         self.cov = np.array([[0.0001, 0.0001, 0.0001],
                              [0.0001, 0.0001, 0.0001],
                              [0.0001, 0.0001, 0.0001]])
@@ -131,9 +129,9 @@ class Filter(Node):
         msg.pose = PoseWithCovariance()
 
         msg.pose.pose = Pose()
-        msg.pose.pose.position.x = float(self.state[0][0])
-        msg.pose.pose.position.y = float(self.state[1][0])
-        quat = euler_to_quaternion(0, 0, self.state[2][0])
+        msg.pose.pose.position.x = float(self.state[0])
+        msg.pose.pose.position.y = float(self.state[1])
+        quat = euler_to_quaternion(0, 0, self.state[2])
         msg.pose.pose.orientation.x = float(quat[0])
         msg.pose.pose.orientation.x = float(quat[1])
         msg.pose.pose.orientation.x = float(quat[2])
@@ -149,15 +147,15 @@ class Filter(Node):
         msg.pose.covariance[1][1] = self.cov[1][1]
         msg.pose.covariance[5][5] = self.cov[2][2]
         msg.pose.covariance = msg.pose.covariance.reshape(-1)
-        self.get_logger().info('Publishing: "%f" "%f" "%f"' % (self.state[0][0], self.state[1][0], self.state[2][0]))
+        self.get_logger().info('Publishing: "%f" "%f" "%f"' % (self.state[0], self.state[1], self.state[2]))
         self.publisher_.publish(msg)
 
     def predict(self, imu:Imu, dt):
         #predict state
         #self.get_logger().info("predicting")
         w = -1 * imu.angular_velocity.x
-        theta_pred = math.atan2(math.sin(self.state[2][0] + w * dt), math.cos(self.state[2][0] + w * dt))
-        self.state[2][0] = theta_pred
+        theta_pred = math.atan2(math.sin(self.state[2] + w * dt), math.cos(self.state[2] + w * dt))
+        self.state[2] = theta_pred
 
         #predict covariance
         theta_cov = imu.angular_velocity_covariance[8]
@@ -169,10 +167,8 @@ class Filter(Node):
 
     def update(self, odom:PoseWithCovariance):
         theta_p = euler_from_quaternion(odom.pose.orientation)
-        Z = np.array([odom.pose.position.x],
-                     [odom.pose.position.y],
-                     [theta_p])
-        self.get_logger().info("Z: %f, %f, %f" % (Z[0][0], Z[1][0], Z[2][0]))
+        Z = np.array([odom.pose.position.x, odom.pose.position.y, theta_p])
+        self.get_logger().info("Z: %f, %f, %f" % (Z[0], Z[1], Z[2]))
         #odom_cov = np.array(odom.covariance).reshape(6,6)
         #R = np.array([[odom_cov[0][0], odom_cov[0][1], odom_cov[0][5]],
         #              [odom_cov[1][0], odom_cov[1][1], odom_cov[1][5]],
@@ -184,10 +180,10 @@ class Filter(Node):
         S = self.cov + R
         K = self.cov @ np.linalg.inv(S)
         nu = Z - self.state
-        self.state = (self.state.flatten() + (K @ nu.flatten())).reshape(3,)
-        self.get_logger().info("new state: %f %f %f" % (self.state[0][0], self.state[1][0], self.state[2][0]))
-        theta_correct = math.atan2(math.sin(self.state[2][0]), math.cos(self.state[2][0]))
-        self.state[2][0] = theta_correct
+        self.state = self.state + (K @ nu)
+        self.get_logger().info("new state: %f %f %f" % (self.state[0], self.state[1], self.state[2]))
+        theta_correct = math.atan2(math.sin(self.state[2]), math.cos(self.state[2]))
+        self.state[2] = theta_correct
         self.cov = (np.eye(3) - K) @ self.cov
 
 def main(args=None):
